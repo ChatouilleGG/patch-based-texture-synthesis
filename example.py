469c56513239d9750440e2df5638eb1a0b101b82
Chatouille <chatouillegg@gmail.com>
2025-10-06 16:35:10 +0200

    add example.py as per the notebook

---
+++ example.py
@@ -0,0 +1,15 @@
+from makeGif import *
+from patchBasedTextureSynthesis import *
+
+exampleMapPath = "imgs/1.jpg"
+outputPath = "out/1/"
+patchSize = 50 #size of the patch (without the overlap)
+overlapSize = 10 #the width of the overlap region
+outputSize = [300,300]
+
+pbts = patchBasedTextureSynthesis(exampleMapPath, outputPath, outputSize, patchSize, overlapSize, in_windowStep = 5, in_mirror_hor = True, in_mirror_vert = True, in_shapshots = True, in_live_updates = True)
+pbts.resolveAll()
+
+gifOutputPath = "out/outGif.gif"
+
+makeGif(outputPath, gifOutputPath, frame_every_X_steps = 1, repeat_ending = 5)
